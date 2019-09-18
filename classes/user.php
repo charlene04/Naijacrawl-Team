e2c8d8eb5de891794f8c828741f8bc42eb38baa6
HP ENVY 17 <HP ENVY 17@KIZINHO>
2019-09-18 16:34:52 +0100

    DB connection setup using sqlite

---
+++ classes/user.php
@@ -0,0 +1,62 @@
+<?php
+
+include('classes/password.php');
+
+class User extends Password {
+
+    private $_db;
+
+    function __construct($db) {
+        parent::__construct();
+
+        $this->_db = $db;
+    }
+
+    /* user hash */
+
+    private function get_user_hash($email) {
+
+        try {
+            $stmt = $this->_db->prepare('SELECT password, email, id FROM users WHERE email = :email ');
+            $stmt->execute(array('email' => $email));
+
+            return $stmt->fetch();
+        } catch (PDOException $e) {
+            echo '<p class="bg-danger">' . $e->getMessage() . '</p>';
+        }
+    }
+
+    /* login function */
+
+    public function login($email, $password) {
+
+        $row = $this->get_user_hash($email);
+
+        if ($this->password_verify($password, $row['password']) == 1) {
+
+            $_SESSION['loggedin'] = true;
+            $_SESSION["VALID_USER_ID"] = $email;
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['id'] = $row['id'];
+
+            return true;
+        }
+    }
+
+    /* logoutfunction */
+
+    public function logout() {
+        session_destroy();
+    }
+
+    /* user login tracking function */
+
+    public function is_logged_in() {
+        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+            return true;
+        }
+    }
+
+}
+
+?>
---
+++ connection/config.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+  naijacrawl
+ */
+try {
+    $db = new PDO('sqlite:../db/db_users.sqlite3');
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, full_name TEXT, email TEXT, password TEXT)";
+
+    $db->exec($query);
+} catch (PDOException $e) {
+    //show error
+    echo '<p class="bg-danger">' . $e->getMessage() . '</p>';
+    exit;
+}
+
+include('classes/user.php');
+$user = new User($db);
